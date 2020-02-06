413887cc48098626c2be53fd8d4e6a897f23abeb
midzer <midzer@gmail.com>
2020-02-06 18:38:31 +0100

    improve service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,32 @@
-self.addEventListener('install', function(event) {
-    console.log('ServiceWorker installed')
+const version = '1.0.0'
+const cacheName = `feed-dachau-${version}`
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/css/main.css',
+        '/assets/js/bootstrap-native-v4.min.js',
+        '/assets/js/app.js',
+        '/assets/icons/sprite.svg'
+      ])
+      .then(() => self.skipWaiting())
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+        .then(cache => cache.match(event.request, {ignoreSearch: true}))
+        .then(response => {
+            return response || fetch(event.request)
+        })
+    )
+    console.log(event.request.url)
 })
