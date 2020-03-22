7c8fc88cdbd094b7a7537d0ed288638bef31489b
midzer <midzer@gmail.com>
2020-03-22 21:48:14 +0100

    remove dead code

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,47 +35,6 @@ function createSVG (icon) {
   return svg
 }
 
-function checkNotificationPromise () {
-  try {
-    Notification.requestPermission().then();
-  } catch (e) {
-    return false
-  }
-  return true
-}
-
-function handlePermission (permission) {
-  // Whatever the user answers, we make sure Chrome stores the information
-  if (!('permission' in Notification)) {
-    Notification.permission = permission;
-  }
-  // set the button to shown or hidden, depending on what the user answers
-  if (Notification.permission === 'denied' || Notification.permission === 'default') {
-    pushButton.disabled = false
-  }
-  else {
-    pushButton.disabled = true
-  }
-}
-
-function askNotificationPermission() {
-  // function to actually ask the permissions
-  // Let's check if the browser supports notifications
-  if (!'Notification' in window) {
-    console.log('This browser does not support notifications.')
-  }
-  else {
-    if (checkNotificationPromise()) {
-      Notification.requestPermission().then((permission) => handlePermission(permission))
-    }
-    else {
-      Notification.requestPermission(function(permission) {
-        handlePermission(permission)
-      })
-    }
-  }
-}
-
 const ws = new WebSocket('wss://feed-dachau.de/api/ws'),
   feedbox = document.getElementById('feedbox')
 
