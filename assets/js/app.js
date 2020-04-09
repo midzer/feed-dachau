5e35a9958d74a80502f685dd9ad585e53f918f32
midzer <midzer@gmail.com>
2020-04-09 20:58:27 +0200

    move badge clearing back to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -250,3 +250,21 @@ if ('serviceWorker' in navigator) {
     })
   })
 }
+// Set the name of the hidden property and the change event for visibility
+let hidden, visibilityChange
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden"
+  visibilityChange = "visibilitychange"
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden"
+  visibilityChange = "msvisibilitychange"
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden"
+  visibilityChange = "webkitvisibilitychange"
+}
+// Handle page visibility change
+document.addEventListener(visibilityChange, function() {
+  if (!document[hidden] && navigator.clearAppBadge) {
+    navigator.clearAppBadge()
+  }
+}, false)
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '1.4.1'
+const version = '1.4.2'
 const cacheName = `feed-dachau-${version}`
 
 self.addEventListener('install', function(event) {
@@ -51,26 +51,6 @@ self.addEventListener('push', event => {
     icon: '../../android-chrome-192x192.png'
   });
   if (navigator.setAppBadge) {
-    navigator.setAppBadge(++newCount)
+    navigator.setAppBadge()
   }
 })
-// Set the name of the hidden property and the change event for visibility
-let hidden, visibilityChange
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
-  hidden = "hidden"
-  visibilityChange = "visibilitychange"
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden"
-  visibilityChange = "msvisibilitychange"
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden"
-  visibilityChange = "webkitvisibilitychange"
-}
-// Handle page visibility change
-let newCount = 0
-document.addEventListener(visibilityChange, function() {
-  if (!document[hidden] && navigator.clearAppBadge) {
-    navigator.clearAppBadge()
-    newCount = 0
-  }
-}, false)
